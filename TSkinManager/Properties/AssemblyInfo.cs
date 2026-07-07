d9c97593b7abbe86b5c78b68b2638d7d982486fe
Tavstal <tavstal@gmail.com>
2026-07-07 23:06:43 +0200

    Update copyright year in AssemblyInfo.cs to 2024-2026

--- TSkinManager/Properties/AssemblyInfo.cs
+++ TSkinManager/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tavstal")]
 [assembly: AssemblyProduct("TSkinManager")]
-[assembly: AssemblyCopyright("Copyright ©  2025 Tavstal")]
+[assembly: AssemblyCopyright("Copyright ©  2024-2026 Tavstal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("en")]
 
